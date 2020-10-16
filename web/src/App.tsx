47198d67c67654dcd25790391786f09033356043
Pedro Morais <pedro.s.morais@gmail.com>
2020-10-15 23:03:24 -0300

    recommited

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+import Routes from './routes'
+import './styles/global.css'
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
